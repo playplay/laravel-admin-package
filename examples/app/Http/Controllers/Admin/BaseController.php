db6bb5544d86a35ee1a109b18ab8b18653241f1f
Mathieu TUDISCO <mathieu.tudisco@neoxia.com>
2017-03-14 16:38:56 +0100

    abstract class BaseController

--- examples/app/Http/Controllers/Admin/BaseController.php
+++ examples/app/Http/Controllers/Admin/BaseController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use LaravelAdminPackage\App\Http\Controllers\BaseController as AdminController;
 
-class BaseController extends AdminController
+abstract class BaseController extends AdminController
 {
     //
 }
