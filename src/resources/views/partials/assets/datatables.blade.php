23ecb2f482d8bc59669a94e8a85bf39d9822cb2b
Alexandre Tobia <alexandre.tobia@gmail.com>
2017-03-20 16:22:45 +0100

    Edit liens assets datatables

--- src/resources/views/partials/assets/datatables.blade.php
+++ src/resources/views/partials/assets/datatables.blade.php
@@ -1,8 +1,7 @@
 @push('styles')
-{{ Html::style('assets/admin/vendor/plugins/datatables/dataTables.bootstrap.css') }}
+{{ Html::style('assets/admin/vendor/datatables/dataTables.min.css') }}
 @endpush
 
 @push('scripts')
-{{ Html::script('assets/admin/vendor/plugins/datatables/jquery.dataTables.min.js') }}
-{{ Html::script('assets/admin/vendor/plugins/datatables/dataTables.bootstrap.min.js') }}
+{{ Html::script('assets/admin/vendor/datatables/datatables.min.js') }}
 @endpush
