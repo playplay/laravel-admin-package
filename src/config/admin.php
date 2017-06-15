30b82a74e98cb002275d709f644d90ef778144f5
Alexandre Tobia <alexandre.tobia@neoxia.com>
2017-06-15 16:58:38 +0200

    Override de la methode toMail afin de definir une route personalisee

--- src/config/admin.php
+++ src/config/admin.php
@@ -15,6 +15,7 @@ return [
 
     // Fully qualified namespace of the User model
     'user_model_fqn'       => '\App\Models\User',
+    'user_passwor_reset_route' => 'auth.passwordReset',
 
     // Should we use the default package auth routes and controllers?
     'use_default_auth'     => true,
